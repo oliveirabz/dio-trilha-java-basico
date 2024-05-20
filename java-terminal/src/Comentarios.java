e60bea1b9459ea1b5370e1d16d20dc54bc513f37
oliveirabz <brunabzoliv@gmail.com>
2024-05-20 15:51:37 -0300

    testando funcionalidades no terminal

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,19 @@
+public class Comentarios {
+    // Testando comentários - one line
+
+    /**
+     * Testando
+     * Multi line
+     * hehehe
+     */
+
+    /**
+     * Este método foi elaborado as pressas
+     * por isso eu abrevei o nome das variáveis
+     * mas olha, ele tem a finalidade de somar ou multiplicar
+     * dois números
+     * 
+     */
+    public void metodo() {
+    }
+}
